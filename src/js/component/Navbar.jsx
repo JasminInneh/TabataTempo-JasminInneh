3fdcba4016c80b23def81ca5be91ac51629f109e
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-08-13 02:38:29 +0000

    changed the navbar

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -3,13 +3,26 @@ import "../../img/TabataTempoLogo.png";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark navbar-dark p-3">
-      <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1" style={{ fontSize: "24px" }}>
-          Tabata Tempo
-        </span>
-      </div>
-    </nav>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    TabataTempo
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Login</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Sign Up</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
   );
 };
 
